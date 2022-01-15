373c552d39cd283b5cb7a359c7c7d3f4798a1ee5
Prashant Chaudhary <Prashantchy265@gmail.com>
2022-01-15 10:12:37 +0545

    [WIP]

--- server/routes/api/auth.route.js
+++ server/routes/api/auth.route.js
@@ -5,16 +5,14 @@ require("dotenv").config();
 const router = express.Router();
 
 router.route("/facebook").get(
-  passport.authenticate("facebook", {
-    scope: ["profile"],
-  })
+  passport.authenticate("facebook")
 );
 
 //callback route
 //hand control to passport to get profile info from query string
 router.route("/facebook/cb").get(passport.authenticate("facebook"), (req, res) => {
-  // res.send(req.user);
-  res.redirect('/');
+  res.send(req.user);
+  // res.redirect('/');
 });
 
 module.exports = router;
